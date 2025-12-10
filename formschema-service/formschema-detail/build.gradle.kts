f6e146f148bcf67474d0f943b78c4a4610bf493d
saviline <saviline@hotmail.com>
2025-12-10 13:43:23 +0100

    chore: added dependencies for testcontainers

--- formschema-service/formschema-detail/build.gradle.kts
+++ formschema-service/formschema-detail/build.gradle.kts
@@ -14,6 +14,9 @@ dependencyManagement {
 
 
 dependencies{
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.19.7"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mongodb")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation(project(":formschema-core"))
